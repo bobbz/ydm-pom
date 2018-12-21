d18642d863974e162b782b505cff73d9a328e961
baozhuang <bz0915@gmail.com>
2018-12-21 15:45:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ class FlashCatLogin(unittest.TestCase):
 
     def test_login(self):
         login_page = page.LoginPage(self.driver)
-        assert (login_page.title == "云电猫后台管理系统"), "登陆页面加载失败"
+        assert (login_page.title == "************"), "登陆页面加载失败"
         login_page.signin("*****","******").logout()
         #assert self.driver.current_url.endswith("main"), "登陆失败"
 
